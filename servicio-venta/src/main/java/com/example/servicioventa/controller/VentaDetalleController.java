d00f8a3ea39379634f8b24617ed36d8cd3ed0fb0
ZethGecko <nelsonluque098@gmail.com>
2025-05-13 15:53:22 -0500

    configuracion

--- servicio-venta/src/main/java/com/example/servicioventa/controller/VentaDetalleController.java
+++ servicio-venta/src/main/java/com/example/servicioventa/controller/VentaDetalleController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/ventasdetalles")
+@RequestMapping("/ventasdetalle")
 public class VentaDetalleController {
 
     @Autowired
