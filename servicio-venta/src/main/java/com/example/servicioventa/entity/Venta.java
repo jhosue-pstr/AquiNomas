e6454cf68d4b0bf90c9172602caf47708351a929
ZethGecko <nelsonluque098@gmail.com>
2025-05-13 14:46:23 -0500

    configuracion

--- servicio-venta/src/main/java/com/example/servicioventa/entity/Venta.java
+++ servicio-venta/src/main/java/com/example/servicioventa/entity/Venta.java
@@ -1,23 +1,17 @@
 package com.example.servicioventa.entity;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
 @Entity
-public class Venta {
+public class venta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private Integer cliente_id;
-    private LocalDateTime fecha_venta;
-    private BigDecimal total;
-    private String estado;
+    private String cargo;
+    private Integer monto;
 
     public Integer getId() {
         return id;
@@ -27,57 +21,31 @@ public class Venta {
         this.id = id;
     }
 
-    public Integer getCliente_id() {
-        return cliente_id;
+    public String getCargo() {
+        return cargo;
     }
 
-    public void setCliente_id(Integer cliente_id) {
-        this.cliente_id = cliente_id;
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
     }
 
-    public LocalDateTime getFecha_venta() {
-        return fecha_venta;
+    public Integer getMonto() {
+        return monto;
     }
 
-    public void setFecha_venta(LocalDateTime fecha_venta) {
-        this.fecha_venta = fecha_venta;
-    }
-
-    public BigDecimal getTotal() {
-        return total;
-    }
-
-    public void setTotal(BigDecimal total) {
-        this.total = total;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public Venta() {
+    public void setMonto(Integer monto) {
+        this.monto = monto;
     }
 
     @Override
     public String toString() {
-        return "Venta{" +
+        return "venta{" +
                 "id=" + id +
-                ", cliente_id=" + cliente_id +
-                ", fecha_venta=" + fecha_venta +
-                ", total=" + total +
-                ", estado='" + estado + '\'' +
+                ", cargo='" + cargo + '\'' +
+                ", monto=" + monto +
                 '}';
     }
 
-    public Venta(Integer id, Integer cliente_id, LocalDateTime fecha_venta, BigDecimal total, String estado) {
-        this.id = id;
-        this.cliente_id = cliente_id;
-        this.fecha_venta = fecha_venta;
-        this.total = total;
-        this.estado = estado;
+    public venta() {
     }
 }
