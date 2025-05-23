f8d39930540af56f49edb1a0485da94bd29193cc
302xdwill <davidom302@gmail.com>
2025-05-23 10:45:42 -0500

    pruebas

--- servicio-compra/src/main/java/com/upeu/serviciocompra/controller/CompraController.java
+++ servicio-compra/src/main/java/com/upeu/serviciocompra/controller/CompraController.java
@@ -1,7 +1,6 @@
 package com.upeu.serviciocompra.controller;
 
 import com.upeu.serviciocompra.dto.CompraDto;
-import com.upeu.serviciocompra.entity.Compra;
 import com.upeu.serviciocompra.service.CompraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,36 +14,43 @@ public class CompraController {
     @Autowired
     private CompraService compraService;
 
+    // Crear compra - retorna CompraDto con detalles y proveedor
     @PostMapping("/solicitud")
-    public Compra crearCompra(@RequestBody CompraDto compraDto) {
+    public CompraDto crearCompra(@RequestBody CompraDto compraDto) {
         return compraService.crearCompra(compraDto);
     }
 
+    // Obtener todas las compras - lista de CompraDto
     @GetMapping
-    public List<Compra> obtenerTodasLasCompras() {
+    public List<CompraDto> obtenerTodasLasCompras() {
         return compraService.obtenerTodasLasCompras();
     }
 
+    // Obtener compra por id - retorna CompraDto
     @GetMapping("/{id}")
-    public Compra obtenerCompraPorId(@PathVariable Integer id) {
+    public CompraDto obtenerCompraPorId(@PathVariable Integer id) {
         return compraService.obtenerCompraPorId(id);
     }
 
+    // Actualizar compra - retorna CompraDto actualizado
     @PutMapping("/{id}")
-    public Compra actualizarCompra(@PathVariable Integer id, @RequestBody CompraDto compraDto) {
+    public CompraDto actualizarCompra(@PathVariable Integer id, @RequestBody CompraDto compraDto) {
         return compraService.actualizarCompra(id, compraDto);
     }
 
+    // Eliminar compra - void
     @DeleteMapping("/{id}")
     public void eliminarCompra(@PathVariable Integer id) {
         compraService.eliminarCompra(id);
     }
 
+    // Validar compra - retorna CompraDto o null
     @PutMapping("/{id}/validar")
-    public Compra validarCompra(@PathVariable Integer id) {
+    public CompraDto validarCompra(@PathVariable Integer id) {
         return compraService.validarCompra(id);
     }
 
+    // Generar orden compra - retorna CompraDto
     @GetMapping("/{id}/orden")
     public CompraDto generarOrdenCompra(@PathVariable Integer id) {
         return compraService.generarOrdenCompra(id);
