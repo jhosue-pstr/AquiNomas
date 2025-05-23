abc0a2f0295c22cd54a18cc5323ffaaa4a5c27c1
302xdwill <davidom302@gmail.com>
2025-05-23 11:01:07 -0500

    pruebas

--- servicio-compra/src/main/java/com/upeu/serviciocompra/ServicioCompraApplication.java
+++ servicio-compra/src/main/java/com/upeu/serviciocompra/ServicioCompraApplication.java
@@ -2,8 +2,12 @@ package com.upeu.serviciocompra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ServicioCompraApplication {
 
     public static void main(String[] args) {
--- servicio-compra/src/main/java/com/upeu/serviciocompra/feign/ProveedorClient.java
+++ servicio-compra/src/main/java/com/upeu/serviciocompra/feign/ProveedorClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "servicio-proveedor", fallback = ProveedorFallback.class)
+@FeignClient(name = "servicio-proveedor")
 public interface ProveedorClient {
     @GetMapping("/proveedores/{id}")
     ProveedorDto getProveedorById(@PathVariable("id") Integer id);
