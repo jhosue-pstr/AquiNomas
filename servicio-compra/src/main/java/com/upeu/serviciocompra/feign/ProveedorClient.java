bd706221f3766b2c2158da457a2b53b64ef7012c
302xdwill <davidom302@gmail.com>
2025-05-25 23:16:05 -0500

    todo de nuevo

--- servicio-compra/src/main/java/com/upeu/serviciocompra/feign/ProveedorClient.java
+++ servicio-compra/src/main/java/com/upeu/serviciocompra/feign/ProveedorClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "servicio-proveedor", url = "http://localhost:5975", fallback = ProveedorFallback.class)
+@FeignClient(name = "servicio-proveedor", url = "http://localhost:5816", fallback = ProveedorFallback.class)
 public interface ProveedorClient {
     @GetMapping("/proveedores/{id}")
     ProveedorDto getProveedorById(@PathVariable("id") Integer id);
